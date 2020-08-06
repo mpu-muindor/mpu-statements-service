0bffb4b00cb1336f0fed200dcc903654644562a0
d11scord <juliakuzkina1@gmail.com>
2020-08-06 14:13:36 +0300

    Refactor requests_teachers urls and views

--- mpu_statements_service/apps/requests_teachers/urls.py
+++ mpu_statements_service/apps/requests_teachers/urls.py
@@ -1,8 +1,9 @@
 """
 Справки преподавателей
 """
-from .views import *
 from django.urls import path
+from .views import *
+from .serializers import *
 
 
 urlpatterns = [
@@ -10,39 +11,39 @@ urlpatterns = [
     path('requests/', RequestHistoryTeacherView.as_view()),
 
     # Получение нового компьютерного оборудования
-    path('new_hardware/', ISSandComputerView.as_view()),
+    path('new_hardware/', RequestTeacherView.as_view(), {'serializer': ISandComputersSerializer}),
     # Подключение компьютера, МФУ, телефона, WiFi
-    path('new_computer/', ISSandComputerView.as_view()),
+    path('new_computer/', RequestTeacherView.as_view(), {'serializer': ISandComputersSerializer}),
     # Обслуживание принтеров, МФУ
-    path('help_mfu/', ISSandComputerView.as_view()),
+    path('help_mfu/', RequestTeacherView.as_view(), {'serializer': ISandComputersSerializer}),
     # Вопрос по СЭД Directum и 1С
-    path('help_1c/', ISSandComputerView.as_view()),
+    path('help_1c/', RequestTeacherView.as_view(), {'serializer': ISandComputersSerializer}),
     # Вопрос по Личному кабинету
-    path('help_lk/', ISSandComputerView.as_view()),
+    path('help_lk/', RequestTeacherView.as_view(), {'serializer': ISandComputersSerializer}),
     # Прочее ИТ-обслуживание
-    path('other_it/', ISSandComputerView.as_view()),
+    path('other_it/', RequestTeacherView.as_view(), {'serializer': ISandComputersSerializer}),
 
     # Справка с места работы
-    path('work_request/', WorkRequestsView.as_view()),
+    path('work_request/', RequestTeacherView.as_view(), {'serializer': WorkRequestsSerializer}),
     # Справка на визу
-    path('work_visa/', WorkRequestsView.as_view()),
+    path('work_visa/', RequestTeacherView.as_view(), {'serializer': WorkRequestsSerializer}),
     # Справка о стаже работы
-    path('work_expirience/', WorkRequestsView.as_view()),
+    path('work_expirience/', RequestTeacherView.as_view(), {'serializer': WorkRequestsSerializer}),
     # Справка о количестве неиспользованных дней отпуска
-    path('work_holiday/', WorkRequestsView.as_view()),
+    path('work_holiday/', RequestTeacherView.as_view(), {'serializer': WorkRequestsSerializer}),
     # Копия трудовой книжки
-    path('work_tk/', WorkRequestsView.as_view()),
+    path('work_tk/', RequestTeacherView.as_view(), {'serializer': WorkRequestsSerializer}),
     # Копии документов из личного дела
-    path('work_copy/', WorkRequestsView.as_view()),
+    path('work_copy/', RequestTeacherView.as_view(), {'serializer': WorkRequestsSerializer}),
     # Справка о работе на условиях внешнего совместительства для внесения стажа в трудовую книжку
-    path('work_add_exp/', WorkRequestsView.as_view()),
+    path('work_add_exp/', RequestTeacherView.as_view(), {'serializer': WorkRequestsSerializer}),
 
     # Справка по форме 2-НДФЛ
-    path('2-ndfl/', WorkPaymentsView.as_view()),
+    path('2-ndfl/', RequestTeacherView.as_view(), {'serializer': WorkPaymentsSerializer}),
     # Справка о выплате (не выплате) единовременного пособия на рождение ребенка
-    path('payment_child/', WorkPaymentsView.as_view()),
+    path('payment_child/', RequestTeacherView.as_view(), {'serializer': WorkPaymentsSerializer}),
     # Справка об отпуске по уходу за ребенком до 1,5 и 3 лет
-    path('work_child/', WorkRequestsView.as_view()),
+    path('work_child/', RequestTeacherView.as_view(), {'serializer': WorkPaymentsSerializer}),
     # Справка о ежемесячных выплатах сотрудника, находящегося в отпуске по уходу за ребенком (декрет)
-    path('decree/', WorkPaymentsView.as_view()),
+    path('decree/', RequestTeacherView.as_view(), {'serializer': WorkPaymentsSerializer}),
 ]
--- mpu_statements_service/apps/requests_teachers/views.py
+++ mpu_statements_service/apps/requests_teachers/views.py
@@ -4,69 +4,15 @@ from rest_framework.response import Response
 from .serializers import *
 
 
-class ISSandComputerView(views.APIView):
+class RequestTeacherView(views.APIView):
     """
-    Вью для следующих типов справок:
-     - Получение нового компьютерного оборудования
-     - Подключение компьютера, МФУ, телефона, WiFi
-     - Обслуживание принтеров, МФУ
-     - Вопрос по СЭД Directum и 1С
-     - Вопрос по Личному кабинету
-     - Прочее ИТ-обслуживание
+    Вью для получения всех справок
     """
 
-    def post(self, request, format=None):
+    def post(self, request, serializer, format=None):
         user = request.user
         if user.user_type == 'professor':
-            serializer = ISandComputersSerializer(data=request.data, context={"user": user})
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(
-            {'message': 'Данный сервис недоступен для Вашей учетной записи.'},
-            status=status.HTTP_403_FORBIDDEN
-        )
-
-
-class WorkRequestsView(views.APIView):
-    """
-    Вью для следующих типов справок:
-     - Справка с места работы
-     - Справка на визу
-     - Справка о стаже работы
-     - Справка о количестве неиспользованных дней отпуска
-     - Копия трудовой книжки
-     - Копии документов из личного дела
-     - Справка о работе на условиях внешнего совместительства для внесения стажа в трудовую книжку
-     - Справка об отпуске по уходу за ребенком до 1,5 и 3 лет
-    """
-
-    def post(self, request, format=None):
-        user = request.user
-        if user.user_type == 'professor':
-            serializer = WorkRequestsSerializer(data=request.data, context={"user": user})
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(
-            {'message': 'Данный сервис недоступен для Вашей учетной записи.'},
-            status=status.HTTP_403_FORBIDDEN
-        )
-
-
-class WorkPaymentsView(views.APIView):
-    """
-    Вью для следующих типов справок:
-     - Справка по форме 2-НДФЛ
-     - Справка о ежемесячных выплатах сотрудника, находящегося в отпуске по уходу за ребенком (декрет)
-    """
-
-    def post(self, request, format=None):
-        user = request.user
-        if user.user_type == 'professor':
-            serializer = WorkPaymentsSerializer(data=request.data, context={"user": user})
+            serializer = serializer(data=request.data, context={"user": user})
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
