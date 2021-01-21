fa089201603649f7bf0c1851477ed21656a7c861
d11scord <juliakuzkina1@gmail.com>
2021-01-21 22:54:24 +0300

    MFC fixed

--- mpu_statements_service/apps/multifunc_center/admin.py
+++ mpu_statements_service/apps/multifunc_center/admin.py
@@ -6,7 +6,8 @@ from .models import Blank
 
 
 class BlankAdmin(admin.ModelAdmin):
-    list_display = ('title', 'blank', )
+    list_display = ('title', 'blank', 'category')
+    list_filter = ('category', )
     prepopulated_fields = {'slug': ('title', )}
 
     def save_model(self, request, obj, form, change):
